24edbd63e77b73193bf22a5349e0e3945d68c2d6
Ben Ku <benku12803@gmail.com>
2023-01-06 02:38:23 -1000

    fixed Listings model using ImageField

--- auctions/models.py
+++ auctions/models.py
@@ -1,17 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+
+class User(AbstractUser):
+    def __str__(self):
+        return f"{self.username}"
 
 class Listing(models.Model):
+    listing_id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name="owner")
     title = models.CharField(max_length=64)
     description = models.TextField()
     starting_bid = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.URLField()
+    image = models.ImageField(default=None, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Listing ID #{self.listing_id}: {self.title}, {self.date}"
 
 class Bid(models.Model):
     pass
 
 class Comment(models.Model):
-    pass
-
-class User(AbstractUser):
     pass
