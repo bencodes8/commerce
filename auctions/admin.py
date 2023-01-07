d963ec9ab10b0175a2272ed03eb949c502e95c7a
Ben Ku <benku12803@gmail.com>
2023-01-07 05:32:21 -1000

    added genre model

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Listing
+from .models import User, Listing, Genre
 
 # Register your models here.
 admin.site.register(Listing)
 admin.site.register(User)
+admin.site.register(Genre)
--- auctions/models.py
+++ auctions/models.py
@@ -6,14 +6,21 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.username}"
 
+class Genre(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+
 class Listing(models.Model):
     listing_id = models.AutoField(primary_key=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name="owner")
     title = models.CharField(max_length=64)
-    description = models.TextField()
-    starting_bid = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(default=None, blank=True)
+    description = models.TextField(max_length=200)
+    starting_bid = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Starting Bid")
+    image = models.ImageField(default=None, blank=True, verbose_name="Image (Optional)")
     date = models.DateTimeField(auto_now_add=True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, default=None, related_name="genre")
     
     def __str__(self):
         return f"Listing ID #{self.listing_id}: {self.title}, {self.date}"
