67b78a988796537f2575a0beba0430114d19b781
Ben Ku <benku12803@gmail.com>
2023-01-11 10:34:36 -1000

    changed some models and added genre manytomany field

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 class User(AbstractUser):
     def __str__(self):
@@ -16,11 +18,11 @@ class Listing(models.Model):
     listing_id = models.AutoField(primary_key=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name="owner")
     title = models.CharField(max_length=64)
-    description = models.TextField(max_length=200)
-    starting_bid = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Starting Bid")
-    image = models.ImageField(default=None, blank=True, verbose_name="Image (Optional)")
+    description = models.CharField(max_length=255, verbose_name="Description")
+    starting_bid = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Starting Bid", validators=[MinValueValidator(Decimal('0.01'))])
+    image = models.ImageField(upload_to='auctions/media', default=None, blank=True, verbose_name="Image (Optional)")
     date = models.DateTimeField(auto_now_add=True)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, default=None, related_name="genre")
+    genre = models.ManyToManyField(Genre, default=None, related_name="genre")
     
     def __str__(self):
         return f"Listing ID #{self.listing_id}: {self.title}, {self.date}"
