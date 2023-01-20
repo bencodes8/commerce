df4ff9eea4de664d48a98857852efb36aa78d5aa
Ben Ku <benku12803@gmail.com>
2023-01-19 21:28:35 -1000

    added bid path

--- auctions/urls.py
+++ auctions/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("create", views.create, name="create"),
     path("listing/<int:listing_id>", views.listing, name="listing"),
     path("watchlist", views.watchlist, name="watchlist"),
+    path("listing/<int:listing_id>/bid", views.bid, name="bid"),
 ]
