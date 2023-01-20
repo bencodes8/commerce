5ca8a756c17a4b01e4501d8659208aeefaa6d6ab
Ben Ku <benku12803@gmail.com>
2023-01-19 21:28:24 -1000

    fixed bug. multiple genres now successfully saves into database

--- auctions/views.py
+++ auctions/views.py
@@ -6,33 +6,63 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-from .models import User, Listing
+from .models import User, Listing, Genre
 
 class NewListingForm(forms.ModelForm):
     class Meta: 
         model = Listing
-        fields = '__all__'
-        exclude = ['owner']
+        fields = ['title', 'description', 'starting_bid', 'image', 'genres']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.TextInput(attrs={'class': 'form-control'}),
             'starting_bid': forms.NumberInput(attrs={'class': 'form-control'}),
             'image': forms.FileInput(attrs={'class': 'form-control'}),
-            'genres': forms.SelectMultiple(attrs={'class': 'form-select form-select-sm'})  
+            'genres': forms.SelectMultiple(attrs={'class': 'form-select form-select-sm',
+                                                  'required': True})  
         }
 
+# index page
 def index(request):
     listings = Listing.objects.all()
     return render(request, "auctions/index.html", {
         "listings": listings 
     })
 
+# create listing page
+@login_required
+def create(request):
+    if request.method == "POST":
+        form = NewListingForm(request.POST)
+        
+        if form.is_valid():
+            genres = request.POST.getlist('genres')
+            
+            instance = form.save(commit=False)
+            instance.owner = request.user
+            instance.save()
+        
+            created_item = Listing.objects.latest('listing_id')
+            
+            for genres_id in genres:
+                created_item.genres.add(genres_id)
+
+            return HttpResponseRedirect(reverse('index'))
+        
+        else:
+            return render(request, "auctions/create.html", {
+                "form": NewListingForm(request.POST)
+            })
+    return render(request, "auctions/create.html", {
+        "form": NewListingForm()
+    })
+
+# listing item information
 def listing(request, listing_id):
     try:
         listing = Listing.objects.get(pk=listing_id)
     except:
         return HttpResponse('Sorry, directing you to this listing item lead to an error. Perhaps it was deleted?')
-    
+    # adds item to watchlist
     if request.method == "POST":
         watcher = User.objects.get(pk=request.user.id)
         item = Listing.objects.get(pk=listing_id)
@@ -44,6 +74,15 @@ def listing(request, listing_id):
         "listing": listing
     })
 
+# bidding page
+@login_required
+def bid(request, listing_id):
+    if request.method == "POST":
+        pass
+    
+    return render(request, "auctions/bid.html")
+
+# watchlist page
 @login_required
 def watchlist(request):
     user = User.objects.get(pk=request.user.id)
@@ -51,25 +90,6 @@ def watchlist(request):
     return render(request, "auctions/watchlist.html", {
         "watchlist": watchlist
     })
-
-@login_required
-def create(request):
-    if request.method == "POST":
-        form = NewListingForm(request.POST)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.owner = request.user
-            instance.save() 
-            print(request.POST)
-            return HttpResponseRedirect(reverse('index'))
-        else:
-            return render(request, "auctions/create.html", {
-                "form": NewListingForm(request.POST)
-            })
-    return render(request, "auctions/create.html", {
-        "form": NewListingForm()
-    })
-
 
 def login_view(request):
     if request.method == "POST":
