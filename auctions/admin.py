38220278e0b8d07b3ea4c5985be94d270bb9d96b
Ben Ku <benku12803@gmail.com>
2023-01-21 18:57:32 -1000

    Update admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Listing, Genre
+from .models import User, Listing, Genre, Bid, Comment
 
 # Register your models here.
 admin.site.register(Listing)
 admin.site.register(User)
 admin.site.register(Genre)
+admin.site.register(Bid)
+admin.site.register(Comment)
