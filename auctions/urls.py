7480495c1d2dc686db753e37c9fa7a7c769c93a6
Ben Ku <benku12803@gmail.com>
2023-02-05 01:39:24 -1000

    added inactive and search path

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("inactive", views.inactive, name="inactive"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
@@ -11,4 +12,5 @@ urlpatterns = [
     path("listing/<int:listing_id>", views.listing, name="listing"),
     path("watchlist", views.watchlist, name="watchlist"),
     path("search", views.search, name="search"),
+    path("search/<slug:slug>", views.search, name="search")
 ]
