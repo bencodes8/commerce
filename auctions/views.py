aed41b3ba47069e2d99ccff6fe3e20c3b30bb96f
Ben Ku <benku12803@gmail.com>
2023-02-04 16:59:06 -1000

    finished listing logic

--- auctions/views.py
+++ auctions/views.py
@@ -6,14 +6,21 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import User, Listing, Genre, Bid
-from .forms import NewListingForm, BidForm, SearchForm
+from .models import User, Listing, Bid, Comment
+from .forms import NewListingForm, BidForm, SearchForm, CommentForm
 
 # index page
 def index(request):
-    listings = Listing.objects.all()
+    active_listings = Listing.objects.all()
     return render(request, "auctions/index.html", {
-        "listings": listings
+        "listings": active_listings
+    })
+
+# inactive listings page
+def inactive(request):
+    inactive_listings = Listing.objects.filter(status=False)
+    return render(request, "auctions/inactive.html", {
+        "listings": inactive_listings
     })
 
 # create listing page
@@ -52,91 +59,110 @@ def listing(request, listing_id):
         listing = Listing.objects.get(pk=listing_id)
     except:
         messages.error(request, 'Sorry, directing you to this listing item lead to an error. Perhaps it was deleted?')
-
-    if request.method == "POST":
-        # bid logic
-        if 'bid_amount' in request.POST:
-            bid_form = BidForm(request.POST)
-            if bid_form.is_valid():
-                # keep track of user's bid
-                bid_input = float(request.POST['bid'])
-                
-                # check to see if user's bid is at least higher than starting bid
-                if bid_input >= float(listing.starting_bid):
-                    
-                    # check if the user has already submitted a bid for this listing or if there are no bids placed yet for this listing
-                    if (not Bid.objects.filter(bidder=request.user.id).exists()) or (not Bid.objects.filter(listing=listing_id).exists()):
-                        instance = bid_form.save(commit=False)
-                        instance.bidder = request.user
-                        instance.listing = listing
-                        instance.bid = bid_input
-                        instance.save()
-                        # test this
-                        if listing.highest_bid is None:
-                            listing.highest_bid = Bid.objects.latest('bidder')
-                            listing.save()
-                            
-                        messages.success(request, f'Successfully placed ${bid_input:.2f} for this listing!')
-                        
-                    else:
-                        bidder = Bid.objects.filter(bidder=request.user.id).filter(listing=listing_id)     
-                        
-                        if bid_input < listing.highest_bid.bid:
-                            messages.error(request, f'Unsuccessful! Bid placed of ${bid_input:.2f} is not higher than current highest bid' )
-                        elif bid_input == listing.highest_bid.bid:
-                            messages.error(request, f'Unable to update current bid, you bidded the same amount as your previous of ${bid_input:.2f}')
-                        else:
-                            bidder.update(bid=bid_input)
-                            listing.save()
-                            messages.success(request, f'Successfully updated current bid to ${bid_input:.2f}')
-                            return render(request, "auctions/listing.html", {
-                                "listing": listing,
-                                "bid_form": BidForm() })
-                    
-                else:
-                    messages.error(request, f'Unsuccessful! Bid placed of ${bid_input:.2f} is not equal to or higher than starting bid')   
+    
+    if not request.user.is_authenticated:
+        return render(request, "auctions/listing.html", {
+            'listing': listing,
+            'comments': Comment.objects.filter(listing=listing_id)
+        })   
+    else:
+        user = User.objects.get(pk=request.user.id)
         
-        # add listing to watchlist
-        elif 'add_to_watchlist' in request.POST:
-            item = Listing.objects.get(pk=listing_id)
-            user = User.objects.get(pk=request.user.id)
-            
-            if not User.objects.filter(watchlist=item).exists():
-                    user.watchlist.add(item)
-                    user.save()
-                    messages.success(request, 'Successfully added listing to watchlist')
-                    return HttpResponseRedirect(reverse('watchlist'))
-            else:
-                messages.error(request, 'Listing already exists on watchlist')
 
-    return render(request, "auctions/listing.html", {
-        "listing": listing,
-        "bid_form": BidForm(),
-    })
+        if request.method == "POST":
+            # bid logic
+            if 'bid_amount' in request.POST:
+                bid_form = BidForm(request.POST)
+                
+                if bid_form.is_valid():
+                    
+                    # keep track of user's bid
+                    bid_input = float(request.POST['bid'])
+                    
+                    # check to see if user's bid is at least higher than starting bid
+                    if bid_input >= float(listing.starting_bid):
+                        
+                        # check if the user has already submitted a bid for this listing or if there are no bids placed yet for this listing
+                        if (not Bid.objects.filter(bidder=request.user.id).exists()) or (not Bid.objects.filter(listing=listing_id).exists()):
+                            instance = bid_form.save(commit=False)
+                            instance.bidder = request.user
+                            instance.listing = listing
+                            instance.bid = bid_input
+                            instance.save()
+                            # set the latest bid to the most current
+                            if listing.highest_bid is None:
+                                listing.highest_bid = Bid.objects.filter(listing=listing_id).latest('bidder')
+                                listing.save()
+                                
+                            messages.success(request, f'Successfully placed ${bid_input:.2f} for this listing!')
+                            
+                        else:
+                            bidder = Bid.objects.filter(bidder=request.user.id).filter(listing=listing_id)     
+                            
+                            if bid_input < listing.highest_bid.bid:
+                                messages.error(request, f'Unsuccessful! Bid placed of ${bid_input:.2f} is not higher than current highest bid' )
+                            elif bid_input == listing.highest_bid.bid:
+                                messages.error(request, f'Unable to update current bid, the highest bid of ${bid_input:.2f} is the same amount bidded')
+                            else:
+                                bidder.update(bid=bid_input)
+                                listing.save()
+                                messages.success(request, f'Successfully updated current bid to ${bid_input:.2f}')
+                                return HttpResponseRedirect(reverse('listing', args=[listing.listing_id]))
+                    else:
+                        messages.error(request, f'Unsuccessful! Bid placed of ${bid_input:.2f} is not equal to or higher than starting bid')   
+        
+            # add listing to watchlist
+            elif 'add_watchlist' in request.POST:
+                
+                if not User.objects.filter(pk=request.user.id).filter(watchlist=listing).exists():
+                        user.watchlist.add(listing)
+                        user.save()
+                        messages.success(request, 'Successfully added listing to watchlist')
+                        return HttpResponseRedirect(reverse('listing', args=[listing.listing_id]))
+                else:
+                    messages.error(request, 'Listing already exists on watchlist')
+         
+            # remove listing from watchlist
+            elif 'remove_watchlist' in request.POST:
+                try: 
+                    user.watchlist.remove(listing)
+                except:
+                    messages.error(request, 'Sorry unable to remove listing from watchlist')
+                messages.success(request, 'Successfully removed item from Watchlist')
+            
+            # close listing
+            elif 'close_listing' in request.POST:
+               listing.status = False
+               listing.save()
+        
+            # comments
+            elif 'comment_send' in request.POST:
+                comment = Comment.objects.create(listing=listing, commenter=request.user, comment=request.POST['comment'])
+                comment.save()
+            
+                
+        return render(request, "auctions/listing.html", {
+            "listing": listing,
+            "bid_form": BidForm(),
+            "comment_form": CommentForm(),
+            "user": user,
+            "watchlist": user.watchlist.all(),
+            "comments": Comment.objects.filter(listing=listing_id)
+        })
 
 # watchlist page
 @login_required
 def watchlist(request):
     user = User.objects.get(pk=request.user.id)
     watchlist = user.watchlist.all()
-    
-    if request.method == "POST":
-        item_id = request.POST['listing_id']
-        try:
-            user.watchlist.remove(item_id)
-        except:
-            messages.error(request, 'Unable to remove from Watchlist')
-        
-        messages.success(request, 'Successfully removed item from Watchlist')
             
     return render(request, "auctions/watchlist.html", {
         "watchlist": watchlist
     })
 
 def search(request):
-    search_form = SearchForm()
     return render(request, "auctions/search.html", {
-        'form': search_form
+        'search_form': SearchForm()
     })
 
 def login_view(request):
