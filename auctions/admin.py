7d276da3fcd3931ebfedf51ab1804e1e9aadba00
Ben Ku <benku12803@gmail.com>
2023-02-05 18:11:52 -1000

    updated admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import User, Listing, Genre, Bid, Comment
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'title', 'description', 'starting_bid')
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('bidder', 'listing', 'bid')
 # Register your models here.
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
 admin.site.register(User)
 admin.site.register(Genre)
-admin.site.register(Bid)
+admin.site.register(Bid, BidAdmin)
 admin.site.register(Comment)
